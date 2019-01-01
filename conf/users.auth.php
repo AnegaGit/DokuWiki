fcd8eb262591e49b086a92920443d367bd7a1233
Anega <anegamail@web.de>
2019-01-01 15:24:22 +0100

    Initial

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+baylamon:$1$WY3b2wCn$UbzLtbcDvhJ.vIR61iQxr.:Baylamon:baylamon@web.de:admin,user
